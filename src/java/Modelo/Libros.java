0aedf29bfdc1ab4ff833000a8b136eb74242c230
Fernando Enriquez <2013200959@untels.edu.pe>
2020-08-03 11:58:14 -0500

    VENTAS - Images y constructor agregados

--- src/java/Modelo/Libros.java
+++ src/java/Modelo/Libros.java
@@ -11,4 +11,62 @@ package Modelo;
  */
 public class Libros {
     
+    int idlibro;
+    String titulo;
+    String autor;
+    String editorial;
+    String estado;
+
+    public Libros() {
+    }
+
+    public Libros(int idlibro, String titulo, String autor, String editorial, String estado) {
+        this.idlibro = idlibro;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.estado = estado;
+    }
+
+    public int getIdlibro() {
+        return idlibro;
+    }
+
+    public void setIdlibro(int idlibro) {
+        this.idlibro = idlibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
 }
